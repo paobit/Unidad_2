520a3e76b5cc9fea7455520c1a2ebc531bc20eb4
Paola Ponce <83021036+paobit@users.noreply.github.com>
2022-03-29 20:52:01 +0200

    Add files via upload

---
+++ IconsRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using static Unidad_2.Program;
+
+
+namespace Unidad_2
+{
+    public class IconsRepository : BaseRepository<GeographicIcons, ChallengeContext>, IconsRepository
+    {
+        public IconsRepository(ChallengeContext dbContext) : base(dbContext)
+        {
+        }
+    }
+
+    public class GeographicIcons
+    {
+    }
+   
+}
